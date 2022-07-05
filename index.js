9d96a4818c6510409831ce32a5e62677b4ec815f
dbstjs95 <choji95@naver.com>
2022-07-05 18:03:39 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://admin:moya5681!@cluster0.nigyg.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err)
+      return res.json({
+        success: false,
+        err,
+      });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
